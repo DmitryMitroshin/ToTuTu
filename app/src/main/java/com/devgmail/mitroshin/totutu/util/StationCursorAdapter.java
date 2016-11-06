fe5ca3c6e7f68db3feba29b69dc1219d4a0d1539
Dmitry <mitroshin.dev@gmail.com>
2016-11-06 17:09:09 +0300

    Добавлен адаптер для работы с БД

---
+++ app/src/main/java/com/devgmail/mitroshin/totutu/util/StationCursorAdapter.java
@@ -0,0 +1,52 @@
+package com.devgmail.mitroshin.totutu.util;
+
+// Класс адаптер, преобразует данные запроса, хранящиеся в курсоре,
+// к необходимому для отображения виду.
+// Связывает данные с конкретными виджетами
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.devgmail.mitroshin.totutu.R;
+
+public class StationCursorAdapter extends CursorAdapter {
+
+    private DatabaseHelper mDatabaseHelper;
+    public StationCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    private String mStringCountry;
+    private String mStringStation;
+    private String mStringCity;
+
+    private TextView mTextCountry;
+    private TextView mTextStation;
+    private TextView mTextCity;
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        mTextCountry = (TextView) view.findViewById(R.id.item_text_country);
+        mTextStation = (TextView) view.findViewById(R.id.item_text_station);
+        mTextCity = (TextView) view.findViewById(R.id.item_text_city);
+
+        mStringCountry = cursor.getString(cursor.getColumnIndexOrThrow(mDatabaseHelper.COUNTRY_TITLE));
+        mStringStation = cursor.getString(cursor.getColumnIndexOrThrow(mDatabaseHelper.STATION_TITLE));
+        mStringCity = cursor.getString(cursor.getColumnIndexOrThrow(mDatabaseHelper.CITY_TITLE));
+
+        mTextCountry.setText(mStringCountry);
+        mTextStation.setText(mStringStation);
+        mTextCity.setText(mStringCity);
+    }
+}
